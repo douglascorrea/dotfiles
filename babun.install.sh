4cf0936c01dabb5739a81a79a7ac10cf9fa90d64
Douglas Silvio Correa <me@douglascorrea.io>
2016-02-26 08:24:48 -0300

    .config removed

--- babun.install.sh
+++ babun.install.sh
@@ -1,10 +1,20 @@
-ln -s /c/Users/dsilvio/Sites ~/Sites
-ln -s /c/Users/dsilvio/Sites/dotfiles/.config ~/.config
-ln -s /c/Users/dsilvio/Sites/dotfiles/.gitconfig ~/.gitconfig
-ln -s /c/Users/dsilvio/Sites/dotfiles/.minttyrc ~/.minttyrc
-ln -s /c/Users/dsilvio/Sites/dotfiles/.tmux ~/.tmux
-ln -s /c/Users/dsilvio/Sites/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s /c/Users/dsilvio/Sites/dotfiles/.vim ~/.vim
-ln -s /c/Users/dsilvio/Sites/dotfiles/.vimrc ~/.vimrc
-ln -s /c/Users/dsilvio/Sites/dotfiles/.zshrc ~/.zshrc
-ln -s ~/Sites /c/Users/dsilvio/Sites
+rm -rf ~/.tmux.conf
+rm -rf ~/Sites
+rm -rf ~/.config
+rm -rf ~/.gitconfig
+rm -rf ~/.minttyrc
+rm -rf ~/.tmux
+rm -rf ~/.vim
+rm -rf ~/.vimrc
+rm -rf /etc/vimrc
+rm -rf ~/.zshrc
+ln -s /c/Users/douglas/Sites ~/Sites
+ln -s /c/Users/douglas/Sites/dotfiles/.config ~/.config
+ln -s /c/Users/douglas/Sites/dotfiles/.gitconfig ~/.gitconfig
+ln -s /c/Users/douglas/Sites/dotfiles/.minttyrc ~/.minttyrc
+ln -s /c/Users/douglas/Sites/dotfiles/.tmux ~/.tmux
+ln -s /c/Users/douglas/Sites/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s /c/Users/douglas/Sites/dotfiles/.vim ~/.vim
+ln -s /c/Users/douglas/Sites/dotfiles/.vimrc ~/.vimrc
+ln -s /c/Users/douglas/Sites/dotfiles/.zshrc ~/.zshrc
+ln -s ~/Sites /c/Users/douglas/Sites
