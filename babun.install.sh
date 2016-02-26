7800c1c1418cbe11c083ee96a23853b14658feef
Douglas Silvio Correa <me@douglascorrea.io>
2016-02-26 08:31:26 -0300

    Base16

--- babun.install.sh
+++ babun.install.sh
@@ -8,6 +8,7 @@ rm -rf ~/.vim
 rm -rf ~/.vimrc
 rm -rf /etc/vimrc
 rm -rf ~/.zshrc
+rm -rf ~/.ctags
 ln -s /c/Users/douglas/Sites ~/Sites
 ln -s /c/Users/douglas/Sites/dotfiles/.config ~/.config
 ln -s /c/Users/douglas/Sites/dotfiles/.gitconfig ~/.gitconfig
@@ -17,4 +18,5 @@ ln -s /c/Users/douglas/Sites/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s /c/Users/douglas/Sites/dotfiles/.vim ~/.vim
 ln -s /c/Users/douglas/Sites/dotfiles/.vimrc ~/.vimrc
 ln -s /c/Users/douglas/Sites/dotfiles/.zshrc ~/.zshrc
+ln -s /c/Users/douglas/Sites/dotfiles/.ctags ~/.ctags
 ln -s ~/Sites /c/Users/douglas/Sites
